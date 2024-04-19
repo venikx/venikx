47795366f0fc74c3706fe980684711ff7b8e38f1
Kevin Rangel <code@venikx.com>
2024-04-19 15:04:44 +0200

    chore: add changesets and nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "venikx";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ nodejs_20 ];
+
+          shellHook = with pkgs; ''
+            export PATH="$PWD/node_modules/.bin/:$PATH"
+            echo "node `${nodejs_20}/bin/node --version`"
+          '';
+        };
+      });
+}
